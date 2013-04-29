7d4205e43b0bcdb2bb4ca5e69b7f507ef8d86b35
Alex Birmingham <alexx1523@gmail.com>
2013-04-29 12:35:04 -0700

    A very bland game.

--- curses_test.py
+++ curses_test.py
@@ -1,3 +1,4 @@
+import random
 import curses
 import time
 
@@ -6,59 +7,129 @@ HEIGHT = 15
 FPS    = 60
 SLEEP_TIME = 1.0/FPS
 
-grid = []
-player = { 'x': 2, 'y': 2 }
+class Grid():
+    def __init__(self, width, height):
+        self.EMPTY = -1
+        self.data = [ [self.EMPTY for j in range(width)] for i in range(height) ]
+
+    def is_coord_empty(self, x, y):
+        return self.data[y][x] == self.EMPTY
+
+    def fetch_obj(self, x, y):
+        return self.data[y][x]
+
+    def place_obj(self, obj):
+        self.data[obj.y][obj.x] = obj
+
+    def move_obj(self, obj, moveX, moveY):
+        newX, newY = obj.x + moveX, obj.y + moveY
+
+        if self.is_coord_empty(newX, newY):
+            self.data[obj.y][obj.x] = self.EMPTY
+            obj.x, obj.y = newX, newY
+            self.place_obj(obj)
+
+    def as_list(self):
+        return self.data
+
+    def draw(self, screen):
+        y = 0
+        for horizontal in self.data:
+            screen.addstr(y, 0, ''.join([ ' ' if obj == self.EMPTY else obj.ascii() for obj in horizontal ]))
+            screen.refresh()
+            y += 1
+
+class Enemy():
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.idle_counter = 1
+        self.asciiChar = 'T'
+
+    def ascii(self):
+        return self.asciiChar
+
+    def update(self, grid):
+        self.idle_counter += 1;
+
+        if self.idle_counter % 120 == 0:
+            self.idle_counter = 1
+            movex, movey = 0, 0
+
+            for x in random.sample([-1, 0, 1], 3):
+                for y in random.sample([-1, 0, 1], 3):
+                    if x == 0 and y == 0:
+                        continue
+                    else:
+                        if isinstance(grid.fetch_obj(self.x + x, self.y + y), Player):
+                            # attack
+                            self.asciiChar = '!'
+                            break
+                        elif grid.is_coord_empty(self.x + x, self.y + y):
+                            movex, movey = x, y
+
+            grid.move_obj(self, movex, movey)
+
+class Player():
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def ascii(self):
+        return 'A'
+
+    def update(self, grid):
+        if self.x_move or self.y_move:
+            grid.move_obj(self, self.x_move, self.y_move)
+
+class Wall():
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+    def ascii(self):
+        return '#'
+    def update(self, grid):
+        grid.place_obj(self)
 
 def main(screen):
     screen.nodelay(1)
 
+    grid         = Grid(WIDTH, HEIGHT)
+    player       = Player(1, 1)
+    game_objects = build_walls()
+    game_objects.append(player)
+
+    game_objects.append(Enemy(5, 5)) # temp
+
+    for obj in game_objects:
+        grid.place_obj(obj)
+
     while (True):
         start_time = time.time()
+        capture_input(screen, grid, player);
 
-        getInput(screen);
-        updateGrid();
-        draw(screen);
+        for obj in game_objects:
+            obj.update(grid)
 
+        grid.draw(screen)
         time.sleep(SLEEP_TIME - (time.time() - start_time))
 
-def getInput(screen):
-    global player
-    key = screen.getch()
+def build_walls():
+    return [ Wall(x, y)
+            for x in range(WIDTH)
+            for y in range(HEIGHT)
+            if x == 0 or x == (WIDTH-1)
+            or y == 0 or y == (HEIGHT-1) ]
 
-    oldy = player['y']
-    oldx = player['x']
+def capture_input(screen, grid, player):
+    key = screen.getch()
+    player.x_move = 0
+    player.y_move = 0
 
     if key == curses.KEY_LEFT:
-        player['x'] = player['x'] - 1
+        player.x_move = -1
     elif key == curses.KEY_RIGHT:
-        player['x'] = player['x'] + 1
+        player.x_move = 1
     elif key == curses.KEY_UP:
-        player['y'] = player['y'] - 1
+        player.y_move = -1
     elif key == curses.KEY_DOWN:
-        player['y'] = player['y'] + 1
-
-    # Clamp
-    if (len(grid) and grid[player['y']][player['x']] == '#'):
-        player['y'] = oldy
-        player['x'] = oldx
-
-def updateGrid():
-    global grid
-    grid = ['#' * WIDTH]
-
-    for i in range(HEIGHT-2):
-        grid.append('#' + (' ' * (WIDTH-2)) + '#')
-
-    grid.append('#' * WIDTH)
-
-def draw(screen):
-    for y in range(len(grid)):
-        # Grid
-        screen.addstr(y, 0, grid[y])
-
-        # Player
-        screen.addstr(player['y'], player['x'], 'A')
-
-        screen.refresh()
+        player.y_move = 1
 
 curses.wrapper(main)
